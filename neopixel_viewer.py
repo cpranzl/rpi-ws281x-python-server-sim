519d249c68f78e565fe6df5f151e9b3ead5ee00d
Christoph Pranzl <christoph.pranzl@pranzl.net>
2020-11-24 12:45:19 +0100

    Initial commit

---
+++ neopixel_viewer.py
@@ -0,0 +1,90 @@
+from emulator_backend import Adafruit_NeoPixel
+import time
+
+STRIPE_LENGTH = 8
+RING_LENGTH = 16
+LED_NUMBER = (STRIPE_LENGTH * 2) + (RING_LENGTH * 2)
+LED_BRIGHTNESS = 75
+
+DELAY = 0.05
+
+STRIPE_ONE_START = 0                                    # 0
+RING_ONE_FIRST = STRIPE_LENGTH                          # 8
+RING_ONE_START = STRIPE_LENGTH + 1                      # 9
+RING_ONE_LAST = RING_ONE_FIRST + (RING_LENGTH // 2)     # 16
+RING_TWO_FIRST = STRIPE_LENGTH + RING_LENGTH            # 24
+RING_TWO_START = STRIPE_LENGTH + RING_LENGTH + 1        # 25
+RING_TWO_LAST = RING_TWO_FIRST + (RING_LENGTH // 2)     # 32
+STRIPE_TWO_START = STRIPE_LENGTH + (RING_LENGTH * 2)    # 40
+
+
+def kspowerup(pixels):
+
+    # Stripes from bottom to top
+    for i in range(STRIPE_ONE_START, STRIPE_LENGTH, 1):
+        pixels.setPixelColor(STRIPE_ONE_START + i,pixels.Color(255, 0, 0))
+        pixels.setPixelColor(STRIPE_TWO_START + i,pixels.Color(255, 0, 0))
+        pixels.show()
+        time.sleep(DELAY)
+    # First LED in rings
+    pixels.setPixelColor(RING_ONE_FIRST,pixels.Color(0, 255, 255))
+    pixels.setPixelColor(RING_TWO_FIRST,pixels.Color(0, 255, 255))
+    pixels.show()
+    time.sleep(DELAY)
+    # Rings on both sides simultanously
+    ring_one_led = RING_ONE_START
+    ring_two_led = RING_TWO_START
+    for i in range(14, 0, -2):
+        pixels.setPixelColor((ring_one_led),pixels.Color(0, 0, 255))
+        pixels.setPixelColor((ring_one_led + i),pixels.Color(0, 0, 255))
+        pixels.setPixelColor((ring_two_led),pixels.Color(0, 0, 255))
+        pixels.setPixelColor((ring_two_led + i),pixels.Color(0, 0, 255))
+        pixels.show()
+        time.sleep(DELAY)
+        ring_one_led = ring_one_led + 1
+        ring_two_led = ring_two_led + 1
+    # Last LED in rings
+    pixels.setPixelColor(RING_ONE_LAST,pixels.Color(0, 255, 255))
+    pixels.setPixelColor(RING_TWO_LAST,pixels.Color(0, 255, 255))
+    pixels.show()
+    time.sleep(DELAY)
+
+def kspowerdown(pixels):
+
+    # Last LED in rings
+    pixels.setPixelColor(RING_ONE_LAST,pixels.Color(0, 0, 0))
+    pixels.setPixelColor(RING_TWO_LAST,pixels.Color(0, 0, 0))
+    pixels.show()
+    time.sleep(DELAY)
+    # Rings on both sides simultanously
+    ring_one_led = RING_ONE_LAST - 1
+    ring_two_led = RING_TWO_LAST - 1
+    for i in range(2, 16, 2):
+        pixels.setPixelColor((ring_one_led),pixels.Color(0, 0, 0))
+        pixels.setPixelColor((ring_one_led + i),pixels.Color(0, 0, 0))
+        pixels.setPixelColor((ring_two_led),pixels.Color(0, 0, 0))
+        pixels.setPixelColor((ring_two_led + i),pixels.Color(0, 0, 0))
+        pixels.show()
+        time.sleep(DELAY)
+        ring_one_led = ring_one_led - 1
+        ring_two_led = ring_two_led - 1
+    # First LED in rings
+    pixels.setPixelColor(RING_ONE_FIRST,pixels.Color(0, 0, 0))
+    pixels.setPixelColor(RING_TWO_FIRST,pixels.Color(0, 0, 0))
+    pixels.show()
+    time.sleep(DELAY)
+    # Stripes from top to bottom
+    for i in range(STRIPE_LENGTH -1, -1, -1):
+        pixels.setPixelColor(STRIPE_ONE_START + i,pixels.Color(0, 0, 0))
+        pixels.setPixelColor(STRIPE_TWO_START + i,pixels.Color(0, 0, 0))
+        pixels.show()
+        time.sleep(DELAY)
+    time.sleep(DELAY)
+    
+    
+pixels = Adafruit_NeoPixel(LED_NUMBER,6,"NEO_GRB + NEO_KHZ800")
+pixels.begin()
+pixels.setBrightness(LED_BRIGHTNESS)
+pixels.clear()
+kspowerup(pixels)
+kspowerdown(pixels)
---
+++ ws2812mtsrv.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+import time
+import threading
+import queue
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+from emulator_backend import Adafruit_NeoPixel
+import argparse
+
+HOST = 'localhost'
+PORT = 8000
+
+RING_LENGTH = 24
+LED_NUMBER = RING_LENGTH
+
+DELAY = 0.02 # 50 Fps
+
+RING_ONE_START = 0                      # 0
+RING_ONE_FIRST = RING_ONE_START + 1     # 1
+RING_ONE_HALF = RING_LENGTH // 2        # 12
+RING_ONE_LAST = RING_LENGTH - 1         # 23
+
+LED_BRIGHTNESS = 50
+
+BLACK = (0, 0, 0)
+RED = (255, 0 ,0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+TEAL = (0, 128, 128)
+CYAN = (0, 255, 255) 
+WHITE = (255, 255, 255)
+
+# Restrict to a particular path.
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+
+class Animation(object):
+    """ Animations consist of description, color, value and change """
+    def __init__(self, description, color=BLACK, value=0, change=0):
+        self.description = description
+        self.color = color
+        self.value = value
+        self.change = change
+        print('New request for animation {0} received'.format(description))
+        return
+
+
+# Register an instance; all the methods of the instance are
+# published as XML-RPC methods
+class RemoteProcedures:
+    def addAnimation(self, description, color, value, change):
+        script.put(Animation(description, color))
+        print('Animation {0} added to queue'.format(description))
+        return 'ACK'
+
+    def clrAnimations(self):
+        with animations.mutex:
+            animations.queue.clear()
+        print('Animation queue cleared')
+        return 'ACK'
+
+    def chgBrightness(self, value):
+        script.put(Animation('chgbrightness', value = value))
+        print('Brightness changed to {0}'.format(value))
+        return 'ACK'
+
+
+# Create a class to encapsulate the XMLRPCServer
+class ServerThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.localServer = SimpleXMLRPCServer((HOST, PORT),
+                                              requestHandler=RequestHandler,
+                                              allow_none=True)
+        self.localServer.register_introspection_functions()
+        self.localServer.register_instance(RemoteProcedures())
+
+    def run(self):
+        self.localServer.serve_forever()
+
+
+def wipe(pixels, color):
+    """ Wipe color across strip a pixel at a time """
+    print('Wipe the strip with color {0}'.format(color))
+    for i in range(LED_NUMBER):
+        pixels.setPixelColor(i, color)
+        pixels.show()
+        time.sleep(DELAY)
+
+
+def powerup(pixels, color):
+    """ Phoniebox powerup """
+    print('Powerup sequence with color {0}'.format(color))
+    # First LED in ring
+    pixels.setPixelColor(RING_ONE_START, color)
+    pixels.show()
+    time.sleep(DELAY)
+    # Rings on both sides simultanously
+    ring_one_led = RING_ONE_FIRST
+    for i in range((RING_ONE_LAST - RING_ONE_FIRST), 0, -2):
+        pixels.setPixelColor((ring_one_led), color)
+        pixels.setPixelColor((ring_one_led + i), color)
+        pixels.show()
+        time.sleep(DELAY)
+        ring_one_led = ring_one_led + 1
+    # Last LED in ring
+    pixels.setPixelColor(RING_ONE_HALF, color)
+    pixels.show()
+    time.sleep(DELAY)
+
+def powerdown(pixels, color):
+    """ Phoniebox powerdown """
+    print('Powerdown sequence with color {0}'.format(color))
+    # Last LED in ring
+    pixels.setPixelColor(RING_ONE_HALF, color)
+    pixels.show()
+    time.sleep(DELAY)
+    # Rings on both sides simultanously
+    ring_one_led = RING_ONE_HALF - 1
+    for i in range(2, (RING_ONE_LAST + RING_ONE_FIRST), 2):
+        pixels.setPixelColor((ring_one_led), color)
+        pixels.setPixelColor((ring_one_led + i), color)
+        pixels.show()
+        time.sleep(DELAY)
+        ring_one_led = ring_one_led - 1
+    # First LED in ring
+    pixels.setPixelColor(RING_ONE_START, color)
+    pixels.show()
+    time.sleep(DELAY)
+
+
+def nextsong(pixels, color):
+    """ Play next song in playlist """
+    print('Next sequence with color {0}'.format(color))
+    for i in range(LED_NUMBER):
+        print(i)
+        time.sleep(DELAY)
+
+
+def previoussong(pixles, color):
+    """ Play previous song in playlist """
+    print('Previous sequence with color {0}'.format(color))
+    for i in range(LED_NUMBER):
+        print(i)
+        time.sleep(DELAY)
+
+
+def chgvolume(pixles, color, value, change):
+    """ Volume change """
+    print('Volume change from value {0} to {1} sequence with color {2}'
+          .format(value, (value + change), color))
+    for i in range(LED_NUMBER):
+        print(i)
+        time.sleep(DELAY)
+
+
+def chgbrightness(value):
+    """ Change brightness of the LEDs """
+    print('Brightness change to {0}'.format(value))
+    pixels.setBrightness(value)
+
+
+def carddetected(color):
+    """ A card was detected """
+    print('Card detected sequence with color {0}'.format(color))
+    for i in range(LED_NUMBER):
+        print(i)
+        time.sleep(DELAY)
+
+
+def cardremoved(color):
+    """ A card was removed """
+    print('Card removed sequence with color {0}'.format(color))
+    for i in range(LED_NUMBER):
+        print(i)
+        time.sleep(DELAY)
+
+
+def accelerate(color):
+    """ A pixel is accelerated until all LEDs are filled """
+    print ("ACCELERATE!")
+    for i in range(LED_NUMBER):
+        print(i)
+        time.sleep(DELAY)
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c',
+                        '--clear',
+                        action='store_true',
+                        help='clear the display on exit')
+    args = parser.parse_args()
+
+    # Create the queue
+    script = queue.Queue()
+
+    # Create server thread and start it
+    server = ServerThread()
+    server.start()
+
+    # Queue powerup animation
+    script.put(Animation('powerup', TEAL))
+    script.put(Animation('wipe', BLACK))
+
+    # Create Neopixel object with appropriate configuration
+    pixels = Adafruit_NeoPixel(LED_NUMBER,
+                               6,
+                               "NEO_GRB + NEO_KHZ800")
+
+    # Intialize the library (must be called once before other functions)
+    pixels.begin()
+
+    # Set brightness to predefined value
+    pixels.setBrightness(LED_BRIGHTNESS)
+
+    # Clear all pixels
+    pixels.clear()
+
+    print('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
+    try:
+
+        while True:
+            while not script.empty():
+                animation = script.get()
+                if animation.description == 'wipe':
+                    wipe(pixels, animation.color)
+                elif animation.description == 'powerup':
+                    powerup(pixels, animation.color)
+                elif animation.description == 'powerdown':
+                    powerdown(pixels, animation.color)
+                elif animation.description == 'chgvolume':
+                    chgvolume(pixles,
+                              animation.color,
+                              animation.value,
+                              animation.change)
+                elif animation.description == 'chgbrightness':
+                    chgbrightness(animation.value)
+                else:
+                    pass
+
+    except KeyboardInterrupt:
+        if args.clear:
+            wipe(pixels, BLACK)
